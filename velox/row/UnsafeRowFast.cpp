b8adfac64d7050d29715153800a66412e9d77f11
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-03-27 14:46:23 +0000

    Fix Map value type Row(Array)

--- velox/row/UnsafeRowFast.cpp
+++ velox/row/UnsafeRowFast.cpp
@@ -1071,9 +1071,6 @@ RowVectorPtr deserializeRows(
 
   const size_t nullLength = alignBits(numFields);
   for (auto row = 0; row < numRows; ++row) {
-    if (rawNulls != nullptr && bits::isBitNull(rawNulls, row)) {
-      continue;
-    }
     offsets[row] += nullLength;
   }
 
@@ -1083,9 +1080,13 @@ RowVectorPtr deserializeRows(
       std::vector<char*> nestedData(numRows);
       std::vector<size_t> nestedOffsets(numRows, 0);
       for (auto row = 0; row < numRows; ++row) {
-        const auto offset =
+        const auto isTopLevelNull =
+            rawNulls != nullptr && bits::isBitNull(rawNulls, row);
+        if (!isTopLevelNull) {
+          const auto offset =
             readInt32(data[row] + offsets[row] + sizeof(int32_t));
-        nestedData[row] = data[row] + offset;
+          nestedData[row] = data[row] + offset;
+        }
         offsets[row] += kFieldWidth;
       }
       auto field =
