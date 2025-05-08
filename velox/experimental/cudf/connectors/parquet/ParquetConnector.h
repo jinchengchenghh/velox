898a06b9ce2d3e94c4c071c5ba2db7fb2566fdab
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-05-08 10:07:48 +0100

    Revert "fix build"
    This reverts commit 34906f2f504858bb88b244a947c256e604c0d574.

--- velox/experimental/cudf/connectors/parquet/ParquetConnector.h
+++ velox/experimental/cudf/connectors/parquet/ParquetConnector.h
@@ -20,19 +20,19 @@
 
 namespace facebook::velox::cudf_velox::connector::parquet {
 
-class ParquetConnectorFactory : public velox::connector::ConnectorFactory {
+class ParquetConnectorFactory : public connector::ConnectorFactory {
  public:
   static constexpr const char* kParquetConnectorName = "parquet";
 
   ParquetConnectorFactory()
-      : velox::connector::ConnectorFactory(kParquetConnectorName) {}
+      : connector::ConnectorFactory(kParquetConnectorName) {}
 
   explicit ParquetConnectorFactory(const char* connectorName)
-      : velox::connector::ConnectorFactory(connectorName) {}
+      : connector::ConnectorFactory(connectorName) {}
 
-  std::shared_ptr<velox::connector::Connector> newConnector(
+  std::shared_ptr<connector::Connector> newConnector(
       const std::string& id,
-      std::shared_ptr<const velox::config::ConfigBase> config,
+      std::shared_ptr<const config::ConfigBase> config,
       folly::Executor* ioExecutor = nullptr,
       folly::Executor* cpuExecutor = nullptr) override;
 };
