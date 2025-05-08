b73904248a9aeba31e4a470b1323d26fb1a1ae54
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-05-08 10:07:50 +0100

    Revert "Move ParquetConnector to cpp file to avoid including cudf header"
    This reverts commit 2d26de83a934a2051823dfce82885a5599edadfa.

--- velox/experimental/cudf/connectors/parquet/ParquetConnector.cpp
+++ velox/experimental/cudf/connectors/parquet/ParquetConnector.cpp
@@ -14,81 +14,55 @@
  * limitations under the License.
  */
 
-#include "velox/experimental/cudf/connectors/parquet/ParquetConfig.h"
 #include "velox/experimental/cudf/connectors/parquet/ParquetConnector.h"
-#include "velox/experimental/cudf/connectors/parquet/ParquetDataSink.h"
 #include "velox/experimental/cudf/connectors/parquet/ParquetDataSource.h"
-#include "velox/experimental/cudf/connectors/parquet/ParquetTableHandle.h"
 
-#include <cudf/io/parquet.hpp>
-#include <cudf/io/types.hpp>
-#include <cudf/types.hpp>
-
-using namespace facebook::velox::connector;
-using namespace facebook::velox::connector;
-using namespace facebook::velox::config;
 namespace facebook::velox::cudf_velox::connector::parquet {
 
-namespace {
-class ParquetConnector final : public Connector {
- public:
-  ParquetConnector(
-      const std::string& id,
-      std::shared_ptr<const ConfigBase> config,
-      folly::Executor* executor)
-      : Connector(id),
-        parquetConfig_(std::make_shared<ParquetConfig>(config)),
-        executor_(executor) {
-    LOG(INFO) << "cudf::Parquet connector " << connectorId() << " created.";
-  }
+using namespace facebook::velox::connector;
 
-  std::unique_ptr<DataSource> createDataSource(
-      const std::shared_ptr<const RowType>& outputType,
-      const std::shared_ptr<ConnectorTableHandle>& tableHandle,
-      const std::unordered_map<std::string, std::shared_ptr<ColumnHandle>>&
-          columnHandles,
-      ConnectorQueryCtx* connectorQueryCtx) override final {
-    return std::make_unique<ParquetDataSource>(
-        outputType,
-        tableHandle,
+ParquetConnector::ParquetConnector(
+    const std::string& id,
+    std::shared_ptr<const facebook::velox::config::ConfigBase> config,
+    folly::Executor* executor)
+    : Connector(id),
+      parquetConfig_(std::make_shared<ParquetConfig>(config)),
+      executor_(executor) {
+  LOG(INFO) << "cudf::Parquet connector " << connectorId() << " created.";
+}
+
+std::unique_ptr<DataSource> ParquetConnector::createDataSource(
+    const std::shared_ptr<const RowType>& outputType,
+    const std::shared_ptr<ConnectorTableHandle>& tableHandle,
+    const std::unordered_map<std::string, std::shared_ptr<ColumnHandle>>&
         columnHandles,
-        executor_,
-        connectorQueryCtx,
-        parquetConfig_);
-  }
+    ConnectorQueryCtx* connectorQueryCtx) {
+  return std::make_unique<ParquetDataSource>(
+      outputType,
+      tableHandle,
+      columnHandles,
+      executor_,
+      connectorQueryCtx,
+      parquetConfig_);
+}
 
-  const std::shared_ptr<const ConfigBase>& connectorConfig() const override {
-    return parquetConfig_->config();
-  }
-
-  std::unique_ptr<DataSink> createDataSink(
-      RowTypePtr inputType,
-      std::shared_ptr<ConnectorInsertTableHandle> connectorInsertTableHandle,
-      ConnectorQueryCtx* connectorQueryCtx,
-      CommitStrategy commitStrategy) override final {
-    auto parquetInsertHandle =
-        std::dynamic_pointer_cast<ParquetInsertTableHandle>(
-            connectorInsertTableHandle);
-    VELOX_CHECK_NOT_NULL(
-        parquetInsertHandle,
-        "Parquet connector expecting parquet write handle!");
-    return std::make_unique<ParquetDataSink>(
-        inputType,
-        parquetInsertHandle,
-        connectorQueryCtx,
-        CommitStrategy::kNoCommit,
-        parquetConfig_);
-  }
-
-  folly::Executor* executor() const override {
-    return executor_;
-  }
-
- protected:
-  const std::shared_ptr<ParquetConfig> parquetConfig_;
-  folly::Executor* executor_;
-};
-} // namespace
+std::unique_ptr<DataSink> ParquetConnector::createDataSink(
+    RowTypePtr inputType,
+    std::shared_ptr<ConnectorInsertTableHandle> connectorInsertTableHandle,
+    ConnectorQueryCtx* connectorQueryCtx,
+    CommitStrategy /*commitStrategy*/) {
+  auto parquetInsertHandle =
+      std::dynamic_pointer_cast<ParquetInsertTableHandle>(
+          connectorInsertTableHandle);
+  VELOX_CHECK_NOT_NULL(
+      parquetInsertHandle, "Parquet connector expecting parquet write handle!");
+  return std::make_unique<ParquetDataSink>(
+      inputType,
+      parquetInsertHandle,
+      connectorQueryCtx,
+      CommitStrategy::kNoCommit,
+      parquetConfig_);
+}
 
 std::shared_ptr<Connector> ParquetConnectorFactory::newConnector(
     const std::string& id,
