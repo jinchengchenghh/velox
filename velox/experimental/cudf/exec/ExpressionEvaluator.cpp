72920665f916c510808f902a1a74096bb8d80b82
Chengcheng Jin <Chengcheng.jin@ibm.com>
2025-10-01 12:00:23 +0100

    debug

--- velox/experimental/cudf/exec/ExpressionEvaluator.cpp
+++ velox/experimental/cudf/exec/ExpressionEvaluator.cpp
@@ -807,7 +807,7 @@ class HashFunction : public CudfFunction {
     auto col = cudf::hashing::murmurhash3_x86_32(
         inputTableView, seedValue_, stream, mr);
     std::cout <<"cudf table data type is " << static_cast<int32_t>(col->type().id())<< std::endl;
-    return cudf::cast(col, cudf::data_type(cudf::type_id::INT32), stream, mr);
+    return cudf::cast(col->view(), cudf::data_type(cudf::type_id::INT32), stream, mr);
     // return col;
   }
 
