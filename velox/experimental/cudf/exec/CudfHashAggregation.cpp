05a52f21b624826dbe75fa7a650f7679dc70ccc2
Chengcheng Jin <Chengcheng.jin@ibm.com>
2026-01-23 13:23:23 +0800

    debug

--- velox/experimental/cudf/exec/CudfHashAggregation.cpp
+++ velox/experimental/cudf/exec/CudfHashAggregation.cpp
@@ -133,7 +133,7 @@ struct CountAggregator : cudf_velox::CudfHashAggregation::Aggregator {
       cudf::table_view const& input,
       TypePtr const& outputType,
       rmm::cuda_stream_view stream) override {
-    std::cout <<"count get the step " << core::AggregationNode::StepName::toName(step)<< std::endl;
+    std::cout <<"count get the step " << core::AggregationNode::toName(step)<< std::endl;
     if (exec::isRawInput(step)) {
       std::cout <<"The input is raw inpiut"<< std::endl;
       // For raw input, implement count using size + null count
@@ -539,7 +539,7 @@ auto toAggregators(
     auto const inputIndex = aggInputs[0];
     auto const constant = aggConstants.empty() ? nullptr : aggConstants[0];
     auto const companionStep = getCompanionStep(kind, step);
-    std::cout <<"get companionStep " << core::AggregationNode::StepName::toName(companionStep) << " for kind " << kind << std::endl;
+    std::cout <<"get companionStep " << core::AggregationNode::toName(companionStep) << " for kind " << kind << std::endl;
     const auto originalName = getOriginalName(kind);
     const auto resultType = exec::isPartialOutput(companionStep)
         ? exec::resolveIntermediateType(originalName, aggregate.rawInputTypes)
