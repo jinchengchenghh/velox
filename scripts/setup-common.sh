d7de5783bb90ed9e64e65b24c06ca15e72d81adb
Chengcheng Jin <Chengcheng.jin@ibm.com>
2026-01-27 15:38:26 +0800

    Revert "fix folly"
    This reverts commit 48b75bcf955db848e2839dc39bba85dbc188ea89.

--- scripts/setup-common.sh
+++ scripts/setup-common.sh
@@ -46,7 +46,7 @@ function install_fmt {
 
 function install_folly {
   wget_and_untar https://github.com/facebook/folly/archive/refs/tags/"${FB_OS_VERSION}".tar.gz folly
-  cmake_install_dir folly -DBUILD_SHARED_LIBS="$VELOX_BUILD_SHARED" -DBUILD_TESTS=OFF -DFOLLY_HAVE_INT128_T=ON -DFOLLY_USE_GFLAGS=ON -DGLOG_USE_GFLAGS=ON
+  cmake_install_dir folly -DBUILD_SHARED_LIBS="$VELOX_BUILD_SHARED" -DBUILD_TESTS=OFF -DFOLLY_HAVE_INT128_T=ON
 }
 
 function install_fizz {
